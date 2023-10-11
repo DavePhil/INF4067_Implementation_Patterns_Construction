c8965b7af8c67c9c44b1a09eb96ab1d6e5233b05
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-10-11 08:22:32 +0100

    Implémentation de 1ère version de l'abstract factory
    Dans cette version nous avons familles de produits donc deux fabriques pour chaque famille

---
+++ abstract_factory_1/src/Main.java
@@ -0,0 +1,35 @@
+import factory.IProductFactory;
+import factory.ProductFactory1;
+import factory.ProduitFactory2;
+import produits.produitA.ProduitA;
+import produits.produitB.ProduitB;
+/*
+    Fait par CHEDJOUN KENGUEP Dave Etudiant en Master 1 Genie Logiciel
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // Classe qui cliente qui appel la fabrique pour lui fournir les objets
+        // Initialisation des fabriques pour chaque famille de produits
+        IProductFactory productFactory1 = new ProductFactory1();
+        IProductFactory productFactory2 = new ProduitFactory2();
+        // Initialisation des produits
+        ProduitA produitA;
+        ProduitB produitB;
+
+        System.out.println("************** Debut **************");
+        System.out.println("Utilisation de la premiere fabrique");
+        produitA = productFactory1.getProduitA();
+        produitB = productFactory1.getProduitB();
+        produitA.methodeA();
+        produitB.methodeB();
+        // aller à la ligne
+        System.out.println("");
+        System.out.println("Utilisation de la seconde fabrique");
+        produitA = productFactory2.getProduitA();
+        produitB = productFactory2.getProduitB();
+        produitA.methodeA();
+        produitB.methodeB();
+        System.out.println("************** Fin **************");
+    }
+}
